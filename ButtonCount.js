8ec85d0afcad8c874e46f73819f6e4743ef7cbae
rabraham-7482 <ritasusanabraham@gmail.com>
2023-03-15 16:35:49 -0700

    working

---
+++ ButtonCount.js
@@ -0,0 +1,23 @@
+function change() {
+    this.count += 1;
+    let btn = 
+    btn.innerText = `Times Clicked: ${this.count}`;
+};
+
+class ButtonCount extends HTMLElement {
+    constructor() {
+        super();
+        let count = 0;
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        const btn = document.createElement('button');
+        btn.setAttribute('id', 'my_button');
+        btn.innerText = `Times Clicked: ${count}`;
+        btn.addEventListener("click", change)
+        function change() {
+            count += 1;
+            btn.innerText = `Times Clicked: ${count}`;
+        };
+        shadowRoot.appendChild(btn);
+    }
+}
+customElements.define('my-button', ButtonCount);
